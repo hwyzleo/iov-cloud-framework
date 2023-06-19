ff6bca2f41a3f0bbf538272a9d149dedad44da59
hwyz_leo <hwyz.leo.ye@gmail.com>
2023-06-19 23:38:43 +0800

    功能: 通用模块

---
+++ framework-commons/src/main/java/net/hwyz/iov/cloud/framework/commons/enums/ClientType.java
@@ -0,0 +1,15 @@
+package net.hwyz.iov.cloud.framework.commons.enums;
+
+/**
+ * 客户端类型枚举类
+ *
+ * @author hwyz_leo
+ */
+public enum ClientType {
+
+    /** 手机 **/
+    MP,
+    /** 车机 **/
+    HU;
+
+}
